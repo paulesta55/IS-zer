f8d8842e5015b0bd6a512a76a038a810eb33f39e
paul.estano <paul.estano@ensea.fr>
2018-12-17 08:22:40 -0500

    final

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,8 @@
 # app/bot/example.rb
 require 'facebook/messenger'
 include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+# Facebook::Messenger::Subscriptions.subscribe( access_token: ENV["ACCESS_TOKEN"],
+#   subscribed_fields: %w[])
 #   subscribed_fields: %w[feed mention name]
 
 Bot.on :message do |message|
