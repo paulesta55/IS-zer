d1ba4df270bba617d95f7f518f200f0d7eabc705
paul.estano <paul.estano@ensea.fr>
2018-12-17 08:04:29 -0500

    final

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,10 +1,9 @@
 # app/bot/example.rb
 require 'facebook/messenger'
 include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(
-  access_token: access_token
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 #   subscribed_fields: %w[feed mention name]
-)
+
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
 end
