bcf118708257370ffe0d7c0484d1ab4ccc208918
paul.estano <paul.estano@ensea.fr>
2018-12-17 07:40:01 -0500

    first commit

---
+++ app/bot/bot.rb
@@ -0,0 +1,10 @@
+# app/bot/example.rb
+require 'facebook/messenger'
+include Facebook::Messenger
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: access_token
+#   subscribed_fields: %w[feed mention name]
+)
+Bot.on :message do |message|
+  message.reply(text: 'Hello, human!')
+end
